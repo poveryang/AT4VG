110f0073aa05862254af6c1e9b739554fabe05f4
yjunj-mac <yjunjie@163.com>
2025-03-14 17:04:49 +0800

    Refactor ATInterface for improved exposure control and logging

--- src/at_interface.cpp
+++ src/at_interface.cpp
@@ -1,5 +1,6 @@
 #include "at_interface.h"
-#include "pid_controller.h"
+
+#include <utility>
 #include "logger.h"
 #define LOG(message) Logger::getInstance().log(message)
 
@@ -11,8 +12,6 @@ int min_exp_time = 20; // Minimum exposure time
 int max_exp_gain = 255; // Maximum exposure gain
 int min_exp_gain = 1; // Minimum exposure gain
 int n_iter = 0; // Number of iterations
-
-bool update_states = true;
 
 int GetBestIndex(const std::vector<double> &decode_rates, const std::vector<double> &entropies) {
     int best_idx = 0;
@@ -53,50 +52,6 @@ bool AdjustExposureParams(CamParams &params, const double current_brightness, co
     return false; // Target brightness not yet achieved
 }
 
-bool AdjustExposureParamsWithPID(CamParams &params,
-                                 const double current_brightness,
-                                 const double target_brightness,
-                                 PIDExposureController &pid_controller) {
-    // 动态设置权重
-    const double w_et = 1.0 - (params.exp_time - min_exp_time) / static_cast<double>(max_exp_time - min_exp_time);
-    const double w_eg = 1.0 - w_et;
-    pid_controller.SetWeights(w_et, w_eg);
-
-    // 计算调整值
-    auto [exp_time_adjust, gain_adjust] = pid_controller.Calculate(target_brightness, current_brightness);
-    // int exp_time_step = params.exp_time * exp_time_adjust;
-    // int gain_step = params.exp_gain * gain_adjust;
-
-    // 应用调整值
-    params.exp_time *= (1 + exp_time_adjust);
-    params.exp_gain *= (1 + gain_adjust);
-
-    // 边界约束
-    params.exp_time = std::clamp(params.exp_time, min_exp_time, max_exp_time);
-    params.exp_gain = std::clamp(params.exp_gain, min_exp_gain, max_exp_gain);
-
-    LOG("Adjusted params: exp_time=" + std::to_string(params.exp_time) +
-        ", exp_gain=" + std::to_string(params.exp_gain));
-
-    // 检查终止条件
-    if (std::abs(current_brightness - target_brightness) < 5.0) {
-        LOG("Target brightness achieved: " + std::to_string(target_brightness));
-        return true;
-    }
-
-    if (params.exp_time == max_exp_time && params.exp_gain == max_exp_gain) {
-        LOG("Reached maximum exposure and gain limits.");
-        return true;
-    }
-
-    if (params.exp_time == min_exp_time && params.exp_gain == min_exp_gain) {
-        LOG("Reached minimum exposure and gain limits.");
-        return true;
-    }
-
-    return false;
-}
-
 double CalculateEntropy(const cv::Mat &image) {
     // Convert the image to grayscale
     cv::Mat gray;
@@ -126,32 +81,37 @@ double CalculateEntropy(const cv::Mat &image) {
     return entropy;
 }
 
-ATInterface::ATInterface(const CamConf &cam_conf){
-    cam_conf_ = cam_conf;
+ATInterface::ATInterface(CamConf cam_conf):
+    cam_conf_(std::move(cam_conf)),
+    tolerance_(2.0),
+    max_iterations_(50),
+    pid_controller_(0.8, 0.003, 0.01) {
 
     // Clear log file
-    std::ofstream ofs("at4vg_log.txt", std::ofstream::out | std::ofstream::trunc);
+    std::ofstream ofs("/tmp/at4vg_log.txt", std::ofstream::out | std::ofstream::trunc);
     LOG("ATInterface constructor");
+    LOG("Camera configuration: min_et=" + std::to_string(cam_conf_.min_et) + ", max_et=" + std::to_string(cam_conf_.max_et) +
+        ", min_eg=" + std::to_string(cam_conf_.min_eg) + ", max_eg=" + std::to_string(cam_conf_.max_eg) +
+        ", init_et=" + std::to_string(cam_conf_.init_et) + ", init_eg=" + std::to_string(cam_conf_.init_eg));
 }
 
 void ATInterface::Init(const double target_brightness) {
-    LOG("ATInterface Init with target brightness");
+    LOG("ATInterface Init with target brightness: " + std::to_string(target_brightness));
     target_brightness_ = target_brightness;
-
-    // Initialize the next exposure parameters(midpoint of min and max values)
-    next_param_.exp_time = (cam_conf_.max_et - cam_conf_.min_et) / 2 + cam_conf_.min_et;
-    next_param_.exp_gain = (cam_conf_.max_eg - cam_conf_.min_eg) / 2 + cam_conf_.min_eg;
+    cur_exp_time_ = cam_conf_.init_et;
+    cur_exp_gain_ = cam_conf_.init_eg;
 }
 
 void ATInterface::Init(const std::vector<CamParams> &candidate_params) {
-    LOG("ATInterface Init with candidate params");
-    candidate_params_ = candidate_params;
-
-    LOG("Candidate params:");
+    LOG("ATInterface Init with candidate parameters");
+    LOG("Candidate parameters: ");
     for (const auto &param : candidate_params) {
         LOG("Exposure time: " + std::to_string(param.exp_time) + ", gain: " + std::to_string(param.exp_gain));
     }
 
+    candidate_params_ = candidate_params;
+
+    trial_brt_levels_ = {64, 128, 192};
     average_decode_counts_.resize(candidate_params.size(), 0.0);
     entropies_.resize(candidate_params.size(), 0.0);
     candidate_params_idx_ = 0;
@@ -162,40 +122,107 @@ void ATInterface::Init(const std::vector<CamParams> &candidate_params) {
 }
 
 bool ATInterface::RunWithTargetBrt(const cv::Mat &image) {
-    double current_brightness = CalculateBrightness(image);
+    // Calculate the current brightness level
+    cur_brightness_ = CalculateBrightness(image);
+    n_iter_ += 1;
+    LOG("Iteration " + std::to_string(n_iter_) + ": exp_time=" + std::to_string(cur_exp_time_) + ", gain=" + std::to_string(cur_exp_gain_) +
+    ", cur_brightness=" + std::to_string(std::round(cur_brightness_)) + ", target_brightness=" + std::to_string(std::round(target_brightness_)));
 
-    // 初始化 PID 控制器
-    PIDExposureController pid_controller(0.5, 0.01, 0.005); // 总 Kp, Ki 和 Kd
-
-    if (AdjustExposureParamsWithPID(next_param_, current_brightness, target_brightness_, pid_controller)) {
-        best_param_ = next_param_; // 保存最佳参数
-        return true; // 达到目标或边界条件，终止
+    // If the error is within the tolerance, return true
+    const double error = target_brightness_ - cur_brightness_;
+    if (std::abs(error) < tolerance_) {
+        LOG("Target brightness achieved: " + std::to_string(target_brightness_));
+        best_param_.exp_time = cur_exp_time_;
+        best_param_.exp_gain = cur_exp_gain_;
+        n_iter_ = 0;
+        return true;
     }
 
-    return false; // 继续调整
+    // If the number of iterations exceeds the maximum number of iterations, return true
+    sorted_history_params_[std::fabs(error)] = std::make_pair(cur_exp_time_, cur_exp_gain_);
+    history_brightness_[std::fabs(error)] = cur_brightness_;
+    if (n_iter_ >= max_iterations_) {
+        if (!sorted_history_params_.empty()) {
+            const auto &[et, eg] = sorted_history_params_.begin()->second;
+            cur_exp_time_ = et;
+            cur_exp_gain_ = eg;
+            cur_brightness_ = history_brightness_.begin()->second;
+        }
+        best_param_.exp_time = cur_exp_time_;
+        best_param_.exp_gain = cur_exp_gain_;
+        n_iter_ = 0;
+        LOG("Maximum number of iterations reached, current brightness: " + std::to_string(cur_brightness_));
+        return true;
+    }
+
+    // Else, calculate the adjustment values for exposure gain
+    const auto [adjust_et_factor, adjust_eg_factor] = pid_controller_.Calculate(error);
+    // std::cout << "Adjustment factors: adjust_et_factor=" << adjust_et_factor << ", adjust_eg_factor=" << adjust_eg_factor << std::endl;
+    int adjust_et_step = std::floor(cur_exp_gain_ * adjust_et_factor);
+    int adjust_eg_step = std::floor(cur_exp_gain_ * adjust_eg_factor);
+    // std::cout << "Adjustment steps: adjust_et_step=" << adjust_et_step << ", adjust_eg_step=" << adjust_eg_step << std::endl;
+
+    if (adjust_et_factor != 0 && abs(adjust_et_step) < 20) {
+        adjust_et_step = (error > 0) ? 20 : -20;
+    }
+    if (adjust_eg_factor != 0 && abs(adjust_eg_step) < 2) {
+        adjust_eg_step = (error > 0) ? 2 : -2;
+    }
+
+    // boundary conditions
+    if ((pid_controller_.weight_et == 0 || cur_exp_time_ >= cam_conf_.max_et) && cur_exp_gain_ == cam_conf_.max_eg && adjust_eg_step > 0) {
+        best_param_.exp_time = cur_exp_time_;
+        best_param_.exp_gain = cur_exp_gain_;
+        n_iter_ = 0;
+        LOG("Reached maximum exposure and gain limits.");
+        return true;
+    }
+
+    if ((pid_controller_.weight_et == 0 || cur_exp_time_ <= cam_conf_.min_et) && cur_exp_gain_ == cam_conf_.min_eg && adjust_eg_step < 0) {
+        best_param_.exp_time = cur_exp_time_;
+        best_param_.exp_gain = cur_exp_gain_;
+        n_iter_ = 0;
+        LOG("Reached minimum exposure and gain limits.");
+        return true;
+    }
+
+    cur_exp_time_ += adjust_et_step;
+    cur_exp_gain_ += adjust_eg_step;
+
+    // clip to legal range
+    cur_exp_time_ = std::clamp(cur_exp_time_, cam_conf_.min_et, cam_conf_.max_et);
+    cur_exp_gain_ = std::clamp(cur_exp_gain_, cam_conf_.min_eg, cam_conf_.max_eg);
+    next_param_.exp_time = cur_exp_time_;
+    next_param_.exp_gain = cur_exp_gain_;
+
+    return false;
 }
 
 bool ATInterface::RunWithCandidates(const cv::Mat &image, const int decode_count) {
-    // 1. Record decode rate and entropy
-    // 2. Update exposure parameters
-
+    LOG("RunWithCandidates, capture count: " + std::to_string(cap_count_) + ", candidate params index: " + std::to_string(candidate_params_idx_));
     if (candidate_params_idx_ == candidate_params_.size()) {
         // Check if any candidate achieved 100% decode rate
+        std::cout << "All candidates tested." << std::endl;
         const int best_idx = GetBestIndex(average_decode_counts_, entropies_);
         if (average_decode_counts_[best_idx] == 1.0) {
             best_param_ = candidate_params_[best_idx];
             return true;
         }
-
-        if (!trial_brt_levels_.empty()) {
-            if (const double current_brightness = cv::mean(image)[0];
-                AdjustExposureParams(next_param_, current_brightness, trial_brt_levels_.back())) {
-                candidate_params_.push_back(next_param_);
-                trial_brt_levels_.pop_back(); // Remove target brightness only when achieved
-                n_iter = 0;
-            }
-            return false;
+        std::cout << "Decode rates: ";
+        for (const auto &decode_rate : average_decode_counts_) {
+            std::cout << decode_rate << " ";
         }
+        std::cout << std::endl;
+
+        // if (!trial_brt_levels_.empty()) {
+        //     if (const double current_brightness = cv::mean(image)[0];
+        //         AdjustExposureParams(next_param_, current_brightness, trial_brt_levels_.back())) {
+        //         candidate_params_.push_back(next_param_);
+        //         trial_brt_levels_.pop_back(); // Remove target brightness only when achieved
+        //         n_iter = 0;
+        //     }
+        //     return false;
+        // }
 
         best_param_ = candidate_params_[best_idx];
         return true;
@@ -206,15 +233,10 @@ bool ATInterface::RunWithCandidates(const cv::Mat &image, const int decode_count
     const double entropy = CalculateEntropy(image);
     entropies_[candidate_params_idx_] += entropy;
     cap_count_++;
-    LOG("decode_count: " + std::to_string(decode_count) + ", entropy: " + std::to_string(entropy));
 
     if (cap_count_ == 5) {
         average_decode_counts_[candidate_params_idx_] /= 5.0;
         entropies_[candidate_params_idx_] /= 5.0;
-
-        LOG("Index: " + std::to_string(candidate_params_idx_) +
-            "Decode rate: " + std::to_string(average_decode_counts_[candidate_params_idx_]) +
-            "Entropy: " + std::to_string(entropies_[candidate_params_idx_]));
 
         candidate_params_idx_++;
         cap_count_ = 0;
@@ -247,17 +269,15 @@ bool ATInterface::RunWithCandidates(const cv::Mat &image, const int decode_count
 }
 
 CamParams ATInterface::GetNextParams() {
-    LOG("Next exposure time: " + std::to_string(next_param_.exp_time) + ", gain: " + std::to_string(next_param_.exp_gain));
     return next_param_;
 }
 
 CamParams ATInterface::GetBestParams() {
-    LOG("Best exposure time: " + std::to_string(best_param_.exp_time) + ", gain: " + std::to_string(best_param_.exp_gain));
     return best_param_;
 }
 
 std::string ATInterface::GetVersion() {
-    return "1.0.0";
+    return "1.2.0";
 }
 
 double ATInterface::CalculateBrightness(const cv::Mat &image) {
